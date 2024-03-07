a25a3fa679e14a76b6377421406dd12a3dfc1d69
Smoren <ofigate@gmail.com>
2024-03-07 13:28:12 +0300

    loc indexing with slices

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+import { ArrayCompressSelector, ArraySelector } from './selectors';
+
+export interface ArrayView<T> {
+  readonly loc: SliceArray<T>;
+  readonly length: number;
+
+  toArray(): Array<T>;
+
+  filter(predicate: (value: T) => boolean): ArrayView<T>;
+
+  is(predicate: (value: T) => boolean): ArrayCompressSelector;
+
+  subview(selector: ArraySelector<any>): ArrayView<T>;
+
+  apply(mapper: (item: T, index: number) => T): void;
+
+  set(newValues: Array<T> | ArrayView<T>): void;
+
+  [Symbol.iterator](): IterableIterator<T>;
+}
+
+export type SliceArray<T> = Array<T> & {
+  [index: string]: ArrayView<T>
+}
