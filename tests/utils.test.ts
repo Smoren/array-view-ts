37ea09ab4574f6374660801f02fe0db2b7814ebd
Smoren <ofigate@gmail.com>
2024-03-07 23:51:12 +0300

    refactor tests structure.

--- tests/utils.test.ts
+++ tests/utils.test.ts
@@ -1,5 +1,5 @@
-import { normalizeIndex } from "../../src/utils";
-import { IndexError } from "../../src/excpetions";
+import { normalizeIndex } from "../src/utils";
+import { IndexError } from "../src/excpetions";
 
 describe.each([
   ...dataProviderForSuccess(),
