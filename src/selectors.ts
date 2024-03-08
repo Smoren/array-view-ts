92e1d552afe666084d2a5658ffaff01ec9dcd3ac
Smoren <ofigate@gmail.com>
2024-03-09 02:26:07 +0300

    refactor

--- src/selectors.ts
+++ src/selectors.ts
@@ -2,33 +2,27 @@ import { ArrayCompressView, ArrayIndexListView, ArraySliceView, ArrayView } from
 import { Slice } from "./structs";
 import { ArraySelectorInterface } from "./types";
 
-export abstract class ArraySelector<T> implements ArraySelectorInterface {
-  public readonly value: T;
+export class IndexListSelector implements ArraySelectorInterface {
+  public readonly value: Array<number>;
 
-  public abstract select<U>(source: ArrayView<U>): ArrayView<U>;
-
-  protected constructor(value: T) {
-    this.value = value;
-  }
-}
-
-export class IndexListSelector extends ArraySelector<Array<number>> {
   public select<T>(source: ArrayView<T>): ArrayIndexListView<T> {
     return new ArrayIndexListView<T>(source, { indexes: this.value, readonly: source.readonly });
   }
 
   constructor(value: Array<number> | ArrayView<number>) {
-    super(value instanceof Array ? value : value.toArray());
+    this.value = value instanceof Array ? value : value.toArray();
   }
 }
 
-export class MaskSelector extends ArraySelector<Array<boolean>> {
+export class MaskSelector implements ArraySelectorInterface {
+  public readonly value: Array<boolean>;
+
   public select<T>(source: ArrayView<T>): ArrayCompressView<T> {
     return new ArrayCompressView<T>(source, { mask: this.value, readonly: source.readonly });
   }
 
   constructor(value: Array<boolean> | ArrayView<boolean>) {
-    super(value instanceof Array ? value : value.toArray());
+    this.value = value instanceof Array ? value : value.toArray();
   }
 }
 
