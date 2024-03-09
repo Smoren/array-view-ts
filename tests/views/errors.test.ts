54ce55788475c5377420a368040052ae23476f56
Smoren <ofigate@gmail.com>
2024-03-09 14:22:07 +0300

    symbol error handled and tested.

--- tests/views/errors.test.ts
+++ tests/views/errors.test.ts
@@ -1,7 +1,7 @@
-import { mask, select, slice, view, ArrayView, Slice, IndexError, KeyError } from "../../src";
+import { view, IndexError, KeyError } from "../../src";
 
 describe.each([
-  ...dataProviderForReadIndexError(),
+  ...dataProviderForIndexError(),
 ] as Array<[Array<number>, Array<number>]>)(
   "Array View Read Index Error Test",
   (
@@ -27,7 +27,34 @@ describe.each([
   },
 );
 
-function dataProviderForReadIndexError(): Array<unknown> {
+describe.each([
+  ...dataProviderForIndexError(),
+] as Array<[Array<number>, Array<number>]>)(
+  "Array View Write Index Error Test",
+  (
+    source: Array<number>,
+    indexes: Array<number>,
+  ) => {
+    it("", () => {
+      // Given
+      const v = view(source);
+
+      for (let index of indexes) {
+        try {
+          // When
+          v.loc[index] = 1;
+          fail();
+        } catch (e) {
+          // Then
+          expect(e instanceof IndexError).toBe(true);
+          expect((e as IndexError).message).toEqual(`Index ${index} is out of range.`);
+        }
+      }
+    });
+  },
+);
+
+function dataProviderForIndexError(): Array<unknown> {
   return [
     [[], [-2, -1, 0, 1]],
     [[1], [-3, -2, 1, 2]],
@@ -36,7 +63,7 @@ function dataProviderForReadIndexError(): Array<unknown> {
 }
 
 describe.each([
-  ...dataProviderForReadKeyError(),
+  ...dataProviderForKeyError(),
 ] as Array<[Array<number>, Array<string | Symbol>]>)(
   "Array View Read Key Error Test",
   (
@@ -63,16 +90,47 @@ describe.each([
   },
 );
 
-function dataProviderForReadKeyError(): Array<unknown> {
+describe.each([
+  ...dataProviderForKeyError(),
+] as Array<[Array<number>, Array<string | Symbol>]>)(
+  "Array View Write Key Error Test",
+  (
+    source: Array<number>,
+    indexes: Array<string | Symbol>,
+  ) => {
+    it("", () => {
+      // Given
+      const v = view(source);
+
+      for (let index of indexes) {
+        try {
+          // When
+          // @ts-ignore
+          v.loc[index] = 1;
+          expect(true).toBe(false);
+        } catch (e) {
+          // Then
+          expect(e instanceof KeyError).toBe(true);
+          expect((e as KeyError).message).toEqual(`Invalid key: "${String(index)}".`);
+        }
+      }
+    });
+  },
+);
+
+function dataProviderForKeyError(): Array<unknown> {
   return [
     [[], ['']],
     [[], ['a', 'b', 'c']],
     [[], ['1a', 'test', '!']],
+    [[], [Symbol(''), Symbol('test')]],
     [[1], ['']],
     [[1], ['a', 'b', 'c']],
     [[1], ['1a', 'test', '!']],
+    [[1], [Symbol(''), Symbol('test')]],
     [[1, 2, 3], ['']],
     [[1, 2, 3], ['a', 'b', 'c']],
     [[1, 2, 3], ['1a', 'test', '!']],
+    [[1, 2, 3], [Symbol(''), Symbol('test')]],
   ];
 }
